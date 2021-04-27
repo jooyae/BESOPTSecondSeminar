8d8a530b2775185f83040051055a3fccee6c5d60
JooYae <jooyae3970@naver.com>
2021-04-27 16:41:59 +0900

    _binding

--- app/src/main/java/org/sopt/besoptsecondseminar/FollowingListFragment.kt
+++ app/src/main/java/org/sopt/besoptsecondseminar/FollowingListFragment.kt
@@ -10,12 +10,20 @@ import org.sopt.besoptsecondseminar.databinding.FragmentFollowingListBinding
 
 class FollowingListFragment : Fragment() {
 
+    private var _binding: FragmentFollowingListBinding ?= null
+    private val binding get() = _binding ?: error("View를 참조하기 위해 binding 이 초기화되지 않았습니다.")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentFollowingListBinding.inflate(inflater, container, false)
+        _binding = FragmentFollowingListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
